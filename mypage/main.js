a0609badc667ca9bcad2debe452d7f7ce2e651e4
lauweiyee <1562955061@qq.com>
2022-05-05 17:35:17 +0800

    rebuilding site Thu May  5 17:35:16     2022

--- mypage/main.js
+++ mypage/main.js
@@ -1,14 +1,28 @@
-function showForm(event){
-	event.preventDefault();
-	var contactInfo = document.getElementById("contact-info");
-	var contactForm = document.getElementById("contact-form");
-	contactForm.style.display = "block";
-	contactInfo.style.display = "none";
-}
-function showUserInfo(event){
-	event.preventDefault();
-	var contactInfo = document.getElementById("contact-info");
-	var contactForm = document.getElementById("contact-form");
-	contactForm.style.display = "none";
-	contactInfo.style.display = "block";
-}
+// var daybtn = document.getElementById("customSwitch");
+// var body = document.body;
+// let arrayBgColor = ['#292a2d','#FFF'];
+// let arrayBodyColor = ['#a9a9b3','#161209'];
+// let index = 0;
+// daybtn.onclick = function () {
+// 	body.style.backgroundColor = arrayBgColor[index];
+// 	body.style.color = arrayBodyColor[index];
+// 	index = (index + 1) % array.length;
+// };
+
+
+// const btn = document.getElementById('customSwitch');
+// const body = document.getElementsByName('body');
+// btn.addEventListener('click', function onClick(event) {
+// 	const backgroundColor = body.style.backgroundColor;
+// 	const bodyColor = body.style.color;
+// 	if (backgroundColor === '#292a2d') {
+// 		body.style.backgroundColor = '#FFF';
+// 	} else {
+// 		body.style.backgroundColor = '#292a2d';
+// 	}
+// 	if (bodyColor === '#a9a9b3') {
+// 		body.style.color = '#161209';
+// 	} else {
+// 		body.style.color = '#a9a9b3';
+// 	}
+// });
